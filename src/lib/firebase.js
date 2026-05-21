2fdeb109b1c1db17cb088aecba477e085eedd1b0
Anto-NYC <anto60@live.fr>
2026-05-21 10:21:10 +0200

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyALztD7QZAvUAKJ8I4lxqzcYKtcw5H-P00",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "capitainerie-5498e.firebaseapp.com",
   projectId: "capitainerie-5498e",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "capitainerie-5498e.firebasestorage.app",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "532000161107",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:532000161107:web:7692a47352602aca58f5a6",
 };
 
 const app = initializeApp(firebaseConfig);
